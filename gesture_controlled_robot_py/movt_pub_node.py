ba1fbbdc2c4ca7b0ab220507f01c548b573bd676
Tewodros Berhanu <tediberhanu777@gmail.com>
2023-11-14 11:15:02 +0300

    gesture working

--- gesture_controlled_robot_py/movt_pub_node.py
+++ gesture_controlled_robot_py/movt_pub_node.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
 
-from std_msgs.msg import String
-
+from std_msgs.msg import String, Int32
+import teleop_twist_keyboard
+from geometry_msgs.msg import Twist
 # from gesture import gesture_reciever
 from gesture_controlled_robot_py.hand_gesture_recognition_mediapipe.app import main as gesture_detector
 
@@ -10,20 +11,27 @@ import threading
 
 class MovtPublisher(Node):
     def __init__(self):
+        super().__init__('movement_publisher')
+        self.publisher_ = self.create_publisher(Int32, '/movement/gesture', 10)
         bg_thread = threading.Thread(target=gesture_detector, args=[self.gesture_reciever])
         bg_thread.run()
-        super().__init__('movement_publisher')
-        self.publisher_ = self.create_publisher(String, 'keyboard/keypress', 10)
         # timer_period = 1
         # self.timer = self.create_timer(timer_period, self.timer_callback)
         # self.i = 0      # counter for the callback
 
     def gesture_reciever(self, gesture):
         print(gesture)
-        # msg = String()
-        # msg.data = gesture
-        # self.publisher_.publish(msg)
-        pass
+        msg = Int32()
+        
+        # msg.data = f'{gesture}'
+
+        if gesture == "Go":
+            msg.data = 8
+            # msg.data = 'linear: {x: -0.5}, angular: {z: 0.0}'
+            # msg.linear.x = -0.5
+            # msg.angular.z = 0.0
+        
+        self.publisher_.publish(msg)
 
     def timer_callback(self):
         msg = String()
